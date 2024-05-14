47315b57ae06350a2884b4b19cf05494137c6db6
alexeimarjina <bnaic2002@gmail.com>
2024-05-14 23:27:58 +0300

    fixed

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -10,40 +10,40 @@ const projects = [
         skills: "React project, under the domain name seo.extensive.digital, reCAPTCHA v3",
         description: "API: Google PageSpeed Insights",
         img: seo_checker_app,
-        gitHubLink: "https://github.com/s-user42/seo_tester-react-app",
-        pageLink: "https://seo.extensive.digital"
+        gitHubLink: "https://github.com/stanislavbogatii/seo_tester-react-app",
+        pageLink: "https://saot.seolitte.com"
     },
     {
         title: "Weather App",
         skills: "html/css, React.js and JavaScript.",
         description: "API: Geolocation api, Overpass api, Openweathermap api.",
         img: weather_app,
-        gitHubLink: "https://github.com/s-user42/weather-app-react",
-        pageLink: "https://s-user42.github.io/weather-app-react/"
+        gitHubLink: "https://github.com/stanislavbogatii/weather-app-react",
+        pageLink: "https://stanislavbogatii.github.io/weather-app-react/"
     },
     {
         title: "Marvel App",
         skills: "html/sass, React.js and JavaScript.",
         description: "API: Marvel api. No mobile adaptation.",
         img: marvel_app,
-        gitHubLink: "https://github.com/s-user42/marvel_api_test/tree/main/src",
-        pageLink: "https://s-user42.github.io/marvel_api_test/"
+        gitHubLink: "https://github.com/stanislavbogatii/marvel_api_test/tree/main/src",
+        pageLink: "https://stanislavbogatii.github.io/marvel_api_test/"
     },
     {
         title: "Employeers List App",
         skills: "html/css, React.js and JavaScript.",
         description: "My first mini react app, no mobile adaptation.",
         img: employeers_list,
-        gitHubLink: "https://github.com/s-user42/my-app",
-        pageLink: "https://s-user42.github.io/my-app/"
+        gitHubLink: "https://github.com/stanislavbogatii/my-app",
+        pageLink: "https://stanislavbogatii.github.io/my-app/"
     },
     {
         title: "Crypto Currency App",
         skills: "html/css, React.js and JavaScript.",
         description: "API: Crypto API. Used material-ui library.",
         img: crypto_currency_app,
-        gitHubLink: "https://github.com/s-user42/react_crypto_app",
-        pageLink: "https://s-user42.github.io/react_crypto_app/"
+        gitHubLink: "https://github.com/stanislavbogatii/react_crypto_app",
+        pageLink: "https://stanislavbogatii.github.io/react_crypto_app/"
     }
 ]
 
